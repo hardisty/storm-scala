260711b330197612d43b1f89b1b165793f4136ed
hardisty <frank.hardisty@gmail.com>
2017-11-19 14:35:04 -0800

    scala compliles, storm doesn't work yet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # storm-scala
 Experiments using a storm topology from Scala
+
+Run with
+mvn package exec:java -Dexec.mainClass=hardisty.App
--- src/main/scala/hardisty/App.scala
+++ src/main/scala/hardisty/App.scala
@@ -1,14 +1,20 @@
 package hardisty
 
+
+
 /**
  * @author ${user.name}
  */
 object App {
-  
+  import org.apache.storm.LocalCluster
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
     println( "Hello World!" )
+    var cluster:LocalCluster = new LocalCluster()
+    //println("made a Storm cluster")
+    //cluster.shutdown()
+    //println("shut Storm cluster down")
     println("concat arguments = " + foo(args))
   }
 
